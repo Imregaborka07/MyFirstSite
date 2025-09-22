d4eb5a2ea0c164caa281dce4229200e28ee1386b
Imre Gábor <imregaborka08@gmail.com>
2025-09-22 13:31:29 +0200

    Gyakorlás

---
+++ resources/views/userinput.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div>
+        <h1>Ezt adta meg a felhasználó: {{$input}}</h1>
+    </div>
+
+    <p>Vezetéknév: {{$vnev}}</p>
+    <p>Keresztnév: {{$knev}}</p>
+
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,54 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('/hello', function () {
+    return "Hello world";
+});
+
+Route::get('/valami', function () {
+    return view('valami');
+});
+Route::view(uri: '/contact', view: 'contact');
+
+Route::get('/tomb-visszaad',function (): array{
+   $tomb = ["kulcs" => "érték"];
+   return $tomb;
+});
+
+Route::get("/udvozles",  function () {
+    $nev = "Gábor";
+    $kor = 18;
+    return view("udvozollek", [
+        "nev" => $nev,
+        "kor" => $kor
+    ]);
+
+});
+
+Route::get("/bevasarlolista", function (){
+$lista = [
+    "tejföl",
+    "tej",
+    "liszt",
+    "csoki",
+    "dinnye"
+    ];
+    return view("sajatbevasarlas", [
+        "bevasarlolista" => $lista
+    ]);
+});
+
+Route::get("/felhasznaloiadat", function (){
+    return view("userinput",[
+        "input" => request("felhasznalonev"),
+        "vnev" => request("vezeteknev"),
+        "knev" => request("keresztnev"),
+    ]);
+});
+
+
